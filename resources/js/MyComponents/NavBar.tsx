d540b2586910dea9d872108730e70cbf28cd4c65
wisamahmad07 <wisamahmad786@gmail.com>
2024-03-10 01:10:58 +0500

    bulit colormode switch

--- resources/js/MyComponents/NavBar.tsx
+++ resources/js/MyComponents/NavBar.tsx
@@ -1,12 +1,13 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../../../public/images/logo.webp";
+import ColorModeSwitch from "../MyComponents/ColorModeSwitch";
 
 const NavBar = () => {
     return (
         <>
-            <HStack>
+            <HStack justifyContent="space-between" padding="10px">
                 <Image src={logo} boxSize="60px" />
-                <Text>NavBar</Text>
+                <ColorModeSwitch />
             </HStack>
         </>
     );
