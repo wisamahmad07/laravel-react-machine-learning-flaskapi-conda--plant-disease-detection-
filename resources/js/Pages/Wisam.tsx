dc937f62e3cd22a6b4450e56d41fddbad98d46d7
wisamahmad07 <wisamahmad786@gmail.com>
2024-03-10 00:51:29 +0500

    build a responsive layout

--- resources/js/Pages/Wisam.tsx
+++ resources/js/Pages/Wisam.tsx
@@ -1,9 +1,27 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Grid, GridItem, Show } from "@chakra-ui/react";
 
 const Wisam = () => {
     return (
         <>
-            <Button colorScheme="green">Wisam</Button>
+            <Grid
+                templateAreas={{
+                    base: `"nav" "main"`,
+                    lg: `"nav nav" "sidebar main"`,
+                }}
+            >
+                <GridItem area="nav" bg="green">
+                    navbar
+                </GridItem>
+                <Show above="lg">
+                    <GridItem area="sidebar" bg="red">
+                        sidebar
+                    </GridItem>
+                </Show>
+
+                <GridItem area="main" bg="blue">
+                    main
+                </GridItem>
+            </Grid>
         </>
     );
 };
